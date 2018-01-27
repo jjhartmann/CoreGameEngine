5aa5ba901c6f0da4326c250002fef39e2687e5b1
jhartman <jhartman@sfu.ca>
2018-01-27 17:25:13 -0500

    Add math helpers

---
+++ CoreGameEngine/math_3d.h
@@ -0,0 +1,26 @@
+#pragma once
+
+////////////////////////////////////////////////////////////
+// Header: Math 3D
+////////////////////////////////////////////////////////////
+
+struct Vector3f
+{
+	float x;
+	float y;
+	float z;
+
+	Vector3f()
+	{
+		;
+	}
+
+	Vector3f(float in_x, float in_y, float in_z)
+	{
+		x = in_x;
+		y = in_y;
+		z = in_z;
+	}
+};
+
+
